c3e548c4ff25bc049eb1038e324669510b31ee73
ZhenyingZhu <zz2283@columbia.edu>
2015-08-20 00:31:33 -0700

    Add several tests

---
+++ CppAlgorithms/CppAlgorithms/MapOperations.cpp
@@ -0,0 +1,13 @@
+#include <map>
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int mapOperations()
+{
+	map<string, int>::key_type a("apple"); 
+	cout << a << endl; 
+
+	return 0; 
+}
---
+++ CppAlgorithms/CppAlgorithms/WordsTransform.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <string>
+
+using namespace std; 
+
+ifstream& open_file(ifstream &in, char *file) {
+	in.close(); 
+	in.clear(); 
+	in.open(file); 
+	return in; 
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+		throw runtime_error("wrong number of arguments"); 
+	
+	map<string, string> trans_map; 
+	string key, value; 
+
+	ifstream map_file; 
+	if (!open_file(map_file, argv[1]))
+		throw runtime_error("no transformation file"); 
+	while (map_file >> key >> value) 
+		trans_map.insert(make_pair(key, value)); 
+
+	ifstream input; 
+	if (!open_file(input, argv[2]))
+		throw runtime_error("no input file"); 
+		
+	string line; 
+	while (getline(input, line)) {
+		istringstream stream(line); 
+		string word; 
+		bool first_word = true; 
+		
+		while (stream >> word) {
+			map<string, string>::const_iterator map_it = trans_map.find(word); 
+			if (map_it != trans_map.end())
+				word = map_it->second; 
+
+			if (first_word)
+				first_word = false;
+			else
+				cout << " "; 
+			cout << word; 
+		}
+
+		cout << endl; 
+	}
+	
+	return 0; 
+}
