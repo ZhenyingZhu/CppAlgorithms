40d755cd5add489f3d4bbd53ec32751fa179797f
ZhenyingZhu <zz2283@columbia.edu>
2016-07-09 03:04:31 -0700

    Chapter 15.10, unique_ptr is so hard to use

--- src/epi/chapter15/BinarySearchTree.cpp
+++ src/epi/chapter15/BinarySearchTree.cpp
@@ -25,8 +25,9 @@ using std::next;
 using std::move;
 using std::ostream;
 using myutils::BSTNode;
-using myutils::createPreOrderIntBST;
+using myutils::createBSTFromPreorder;
 using myutils::vec_to_string;
+using std::runtime_error;
 using std::cout;
 using std::endl;
 
@@ -34,23 +35,127 @@ namespace epi {
   namespace chapter15 {
     class BinarySearchTree::BST {
     public:
+        BST(std::unique_ptr<myutils::BSTNode<int>> &tree) {
+            root_ = move(tree);
+        }
+
+        std::unique_ptr<myutils::BSTNode<int>>& getRoot();
+
         bool bstInsert(int key);
 
         bool bstDelete(int key);
 
     private:
-        std::unique_ptr<myutils::BSTNode<int>> root_ = nullptr;
+        std::unique_ptr<myutils::BSTNode<int>> root_;
 
     };
 
+    unique_ptr<BSTNode<int>>& BinarySearchTree::BST::getRoot() {
+        return root_;
+    }
+
+    bool BinarySearchTree::BST::bstInsert(int key) {
+        unique_ptr<BSTNode<int>> node(new BSTNode<int>(key));
+
+        if (root_ == nullptr) {
+            root_ = move(node);
+            return true;
+        }
+
+        BSTNode<int> *cur = root_.get();
+        while (cur) {
+            if (cur->data == key) {
+                return false;
+            } else if (cur->data < key) {
+                if (cur->right == nullptr) {
+                    cur->right = move(node);
+                    return true;
+                } else {
+                    cur = cur->right.get();
+                }
+            } else {
+                if (cur->left == nullptr) {
+                    cur->left = move(node);
+                    return true;
+                } else {
+                    cur = cur->left.get();
+                }
+            }
+        }
+
+        throw runtime_error("Shouldn't reach this code");
+    }
+
+    bool BinarySearchTree::BST::bstDelete(int key) {
+        if (root_ == nullptr)
+            return false;
+
+        BSTNode<int> *node = root_.get(), *prev = nullptr;
+        while (node) {
+            if (node->data == key) {
+                break;
+            } else if (node->data > key) {
+                prev = node;
+                node = node->left.get();
+            } else {
+                prev = node;
+                node = node->right.get();
+            }
+        }
+
+        if (!node)
+            return false;
+
+        if (node->right) {
+            BSTNode<int> *successor = node->right.get(), *preSucc = node;
+            while (successor->left != nullptr) {
+                preSucc = successor;
+                successor = successor->left.get();
+            }
+            node->data = successor->data;
+            if (preSucc == node) {
+                preSucc->right.reset(successor->right.release());
+            } else {
+                preSucc->left.reset(successor->right.release());
+            }
+        } else {
+            if (root_.get() == node) {
+                root_.reset(root_.get()->left.release());
+            } else {
+                if (prev->left.get() == node) {
+                    prev->left.reset(node->left.release());
+                } else {
+                    prev->right.reset(node->left.release());
+                }
+            }
+        }
+
+        return true;
+    }
+
     bool BinarySearchTree::test() {
-        vector<int> array = {2,3,5,7,11,13,17,19,23};
+        vector<int> preOrder = {19, 7, 3, 2, 5, 11, 17, 13, 43, 23, 37, 29, 31, 41, 47, 53};
+        unique_ptr<BSTNode<int>> tree = createBSTFromPreorder(preOrder);
 
-        unique_ptr<BSTNode<int>> res = buildMinHeightBSTFromSortedArray(array);
-        if (res.get()->data != 11) {
-            cout << "Should be " << 11 << endl;
-            cout << "Result " << res.get()->data << endl;
+        BST bst(tree);
 
+        if ( !bst.bstInsert(9) ) {
+            cout << "Failed to insert" << endl;
+            return false;
+        }
+        unique_ptr<BSTNode<int>> &root = bst.getRoot();
+        if (root.get()->left.get()->right.get()->left.get()->data != 9) {
+            cout << "The place for inserted node is not the right node " << endl;
+            return false;
+        }
+
+        if ( !bst.bstDelete(43) ) {
+            cout << "Failed to delete" << endl;
+            return false;
+        }
+
+        if (root.get()->right.get()->data != 47) {
+            cout << "The place for delete node is not the right node " << endl;
             return false;
         }
 
