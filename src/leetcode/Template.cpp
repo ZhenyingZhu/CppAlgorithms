81a03e4065fb66acb7c034eaa4765d3ec0604563
ZhenyingZhu <zz2283@columbia.edu>
2016-09-05 00:46:23 -0700

    Add some codes

---
+++ src/leetcode/Template.cpp
@@ -0,0 +1,24 @@
+/*
+ * Source: https://leetcode.com/problems/
+ * Difficulty: 
+ * Tags: , 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// Solution:
+class Solution {
+};
+
+// Solution:
+/* Java solution
+
+ */
+
+int main() {
+    Solution sol;
+
+    return 0;
+}
