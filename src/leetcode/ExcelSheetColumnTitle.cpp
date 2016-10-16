e75bdbc97695e6a36bd1d905494836589bfcccd4
ZhenyingZhu <zz2283@columbia.edu>
2016-10-16 15:19:08 -0700

    168

---
+++ src/leetcode/ExcelSheetColumnTitle.cpp
@@ -0,0 +1,25 @@
+/*
+ * [Source] https://leetcode.com/problems/excel-sheet-column-title/
+ * [Difficulty]: Easy
+ * [Tag]: Math
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// [Solution]: mode 26, then add a correspond letter until 0.
+// [Corner Case]: should first minus 1
+class Solution {
+};
+
+// [Solution]:
+/* Java solution
+https://github.com/ZhenyingZhu/ClassicAlgorithms/blob/master/src/algorithms/mathandprob/ExcelSheetColumnTitle.java
+ */
+
+int main() {
+    Solution sol;
+
+    return 0;
+}
