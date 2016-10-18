59aae3fbc933e7ceb4b6ed608029967328d8c67a
ZhenyingZhu <zz2283@columbia.edu>
2016-10-18 00:11:48 -0700

    179

--- src/leetcode/LargestNumber.cpp
+++ src/leetcode/LargestNumber.cpp
@@ -1,7 +1,7 @@
 /*
- * [Source] https://leetcode.com/problems/
- * [Difficulty]: 
- * [Tag]: 
+ * [Source] https://leetcode.com/problems/largest-number/
+ * [Difficulty]: Medium
+ * [Tag]: Sort
  */
 
 #include <iostream>
@@ -11,8 +11,8 @@
 
 using namespace std;
 
-// [Solution]:
-// [Corner Case]:
+// [Solution]: First convert numbers to strings. Then sort strings in order like: l=abc,s=de, if abcde > deabc, then l appear first.
+// [Corner Case]: a list of 0 should return one 0
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
@@ -49,8 +49,11 @@ private:
         size_t lSize = larger.size(), sSize = smaller.size();
         if (lSize == 0 || sSize == 0)
             return false;
-        for (size_t i = 0; i < max(lSize, sSize); ++i) {
-            char lC = larger[i % lSize], sC = smaller[i % sSize];
+        size_t l = 0, s = 0;
+        for (size_t i = 0; i < lSize + sSize; ++i) {
+            char lC = l < lSize ? larger[l] : smaller[l - lSize];
+            char sC = s < sSize ? smaller[s] : larger[s - sSize];
+            ++l; ++s;
             if (lC > sC)
                 return true;
             if (lC < sC)
@@ -70,8 +73,10 @@ int main() {
 
     //vector<int> nums = {3, 30, 34, 5, 9};
     //vector<int> nums = {0,0};
-    //vector<int> nums = {824,938,1399,5607,6973,5703,9609,4398,8247};
-    vector<int> nums = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    //1vector<int> nums = {824,938,1399,5607,6973,5703,9609,4398,8247};
+    //vector<int> nums = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    vector<int> nums = {121,12};
+
     cout << sol.largestNumber(nums) << endl;
 
     return 0;
