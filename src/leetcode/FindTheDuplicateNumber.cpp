373909ad3996c384a70507ee672d6c949c006ac6
ZhenyingZhu <zz2283@columbia.edu>
2016-11-16 01:02:11 -0800

    287

---
+++ src/leetcode/FindTheDuplicateNumber.cpp
@@ -0,0 +1,71 @@
+/*
+ * [Source] https://leetcode.com/problems/find-the-duplicate-number/
+ * [Difficulty]: Hard
+ * [Tag]: Binary Search
+ * [Tag]: Array
+ * [Tag]: Two Pointer
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// [Solution]: Use binary search on range not vector element.
+class SolutionBS {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int st = 1, ed = nums.size() - 1; // range from 1 to n
+        while (st + 1 < ed) {
+            int md = st + (ed - st) / 2;
+            if ( numCount(nums, md) > md )
+                ed = md;
+            else
+                st = md;
+        }
+
+        if ( numCount(nums, st) > st )
+            return st;
+        return ed;
+    }
+
+    int numCount(vector<int>& nums, int target) {
+        int cnt = 0;
+        for (int& num : nums) {
+            if (num <= target)
+                ++cnt;
+        }
+        return cnt;
+    }
+};
+
+// [Solution]: Take it as linked list, which link each number from nums[number]. Since range is 1 to n, it means nums[0] never link to itself. Also the duplicate element must create a cycle
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
+
+        int slow = nums[0], fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {1, 2, 3, 4, 5, 2};
+    cout << sol.findDuplicate(nums) << endl;
+
+    return 0;
+}
