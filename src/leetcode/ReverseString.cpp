6f49e6bee6dba55cb04d5dc5e6eb4ae45fff6653
ZhenyingZhu <zz2283@columbia.edu>
2016-11-25 17:02:57 -0800

    344

---
+++ src/leetcode/ReverseString.cpp
@@ -0,0 +1,34 @@
+/*
+ * [Source] https://leetcode.com/problems/
+ * [Difficulty]: 
+ * [Tag]: 
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// [Solution]:
+// [Corner Case]:
+class Solution {
+public:
+    string reverseString(string s) {
+        vector<char> reverse;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            reverse.push_back(s[i]);
+        }
+        return string(reverse.begin(), reverse.end());
+    }
+};
+
+// [Solution]:
+
+int main() {
+    Solution sol;
+
+    cout << sol.reverseString("hello") << endl;
+
+    return 0;
+}
