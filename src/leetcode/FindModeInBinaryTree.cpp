caa2f3e7d31235129c89fdf679cb238678c8000f
ZhenyingZhu <zz2283@columbia.edu>
2017-01-30 22:00:50 -0800

    501

--- src/leetcode/FindModeInBinaryTree.cpp
+++ src/leetcode/FindModeInBinaryTree.cpp
@@ -1,10 +1,12 @@
 /*
- * [Source] https://leetcode.com/problems/find-mode-in-binary-tree/
+ * [Source] https://leetcode.com/problems/find-mode-in-binary-search-tree/
  * [Difficulty]: Easy
- * [Tag]: 
+ * [Tag]: Tree
  */
 
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -17,7 +19,7 @@ struct TreeNode {
 
 // [Solution]: Mode is the number that shows most often than other numbers
 // [Corner Case]:
-class Solution {
+class SolutionONSpace {
 public:
     vector<int> findMode(TreeNode* root) {
         unordered_map<int, int> nodeCnt;
@@ -46,10 +48,62 @@ public:
     }
 };
 
-// [Solution]:
-/* Java solution
+// [Solution]: In order traverse. Maintain a count and current number
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        if (root == NULL)
+            return candidates_;
 
- */
+        // get first node
+        TreeNode *first = root;
+        while (first->left != NULL)
+            first = first->left;
+
+        // use global vars to transmit info
+        num_ = first->val;
+        cnt_ = 0;
+        maxCnt_ = 0;
+
+        inorder(root);
+        // deal with the last number
+        if (cnt_ > maxCnt_)
+            return {num_};
+        if (cnt_ == maxCnt_)
+            candidates_.push_back(num_);
+
+        return candidates_;
+    }
+
+private:
+    void inorder(TreeNode *node) {
+        if (node == NULL)
+            return;
+
+        inorder(node->left);
+
+        // traverse the tree like an array
+        if (node->val == num_) {
+            ++cnt_;
+        } else {
+            if (cnt_ == maxCnt_) {
+            candidates_.push_back(num_);
+            } else if (cnt_ > maxCnt_) {
+                maxCnt_ = cnt_;
+                candidates_ = {num_};
+            }
+            num_ = node->val;
+            cnt_ = 1;
+        }
+
+        inorder(node->right);
+    }
+
+    int num_;
+    int cnt_;
+    int maxCnt_;
+    vector<int> candidates_;
+};
 
 int main() {
     Solution sol;
