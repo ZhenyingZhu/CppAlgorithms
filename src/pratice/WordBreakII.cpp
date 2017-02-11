b18dba17ef2830b75cdfa043aa175e889e64af2a
ZhenyingZhu <zz2283@columbia.edu>
2017-02-11 13:49:18 -0800

    Word Break II DP solution

--- src/pratice/WordBreakII.cpp
+++ src/pratice/WordBreakII.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <unordered_set>
+#include <unordered_map>
 #include <vector>
 #include <climits>
 
 using namespace std;
 
-class Solution {
+// [Source]: https://leetcode.com/problems/word-break-ii/
+// http://www.1point3acres.com/bbs/thread-227554-1-1.html
+
+class SolutionDFS {
 public:
     vector<string> wordBreak(string s, vector<string> &wordDict) {
         if (s.empty() || wordDict.empty())
@@ -50,6 +54,52 @@ public:
         for (int i = 1; i < (int)partial.size(); ++i) {
             res += " " + partial[i];
         }
+        return res;
+    }
+};
+
+// [Solution]: DP
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string> &wordDict) {
+        if (s.empty() || wordDict.empty())
+            return {};
+
+        int minLen = INT_MAX, maxLen = 0;
+        unordered_set<string> dict;
+        for (string &word : wordDict) {
+            minLen = min((int)word.length(), minLen);
+            maxLen = max((int)word.length(), maxLen);
+            dict.insert(word);
+        }
+
+        unordered_map<int, vector<string>> stVecMap;
+        return helper(s, 0, dict, minLen, maxLen, stVecMap);
+    }
+
+    vector<string> helper(string &s, int st, unordered_set<string> &dict, int minLen, int maxLen, unordered_map<int, vector<string>> &stVecMap) {
+        if (st == (int)s.length())
+            return {""};
+        if (stVecMap.count(st))
+            return stVecMap[st];
+
+        vector<string> res;
+        for (int len = minLen; len <= maxLen && st + len <= (int)s.length(); ++len) {
+            string candidate = s.substr(st, len);
+            if (!dict.count(candidate))
+                continue;
+
+            vector<string> partials = helper(s, st + len, dict, minLen, maxLen, stVecMap);
+            if (partials.empty())
+                continue; // left part cannot form any word break
+            for (string &partial : partials) {
+                if (partial.empty())
+                    res.push_back(candidate); // the last word
+                else
+                    res.push_back(candidate + " " + partial);
+            }
+        }
+        stVecMap[st] = res;
         return res;
     }
 };
