9c710b556b7e7cce2e961b7ba20ffa4fd95d8fb7
ZhenyingZhu <zz2283@columbia.edu>
2017-03-05 12:15:22 -0800

    Use array random access

--- src/leetcode/SparseMatrixMultiplication.cpp
+++ src/leetcode/SparseMatrixMultiplication.cpp
@@ -10,9 +10,32 @@
 
 using namespace std;
 
+// [Solution]: Hash map is more expensive than array random access. So this solution is much faster 
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int h1 = A.size(), w1 = A[0].size();
+        int w2 = B[0].size();
+
+        vector<vector<int>> res(h1, vector<int>(w2, 0));
+        for (int i = 0; i < h1; ++i) {
+            for (int j = 0; j < w1; ++j) {
+                if (A[i][j] == 0)
+                    continue;
+
+                for (int k = 0; k < w2; ++k) {
+                    if (B[j][k] != 0)
+                        res[i][k] += A[i][j] * B[j][k];
+                }
+            }
+        }
+        return res;
+    }
+};
+
 // [Solution]: Use Hash map to avoid access each cell several times
 // [Corner Case]:
-class Solution {
+class SolutionHashmap {
 public:
     vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
         if (A.empty() || B.empty())
