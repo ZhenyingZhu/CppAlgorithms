690174565821ab57872ff5acad6ea9c8d7d5a990
ZhenyingZhu <zz2283@columbia.edu>
2017-03-05 00:21:22 -0800

    Use DFS

--- src/leetcode/GraphValidTree.cpp
+++ src/leetcode/GraphValidTree.cpp
@@ -16,9 +16,48 @@
 
 using namespace std;
 
+// [Solution]: DFS
+class Solution {
+public:
+    bool validTree(int n, vector<pair<int, int>>& edges) {
+        unordered_map<int, unordered_set<int>> graph;
+        for (int i = 0; i < n; ++i)
+            graph[i];
+        for (pair<int, int> &edge : edges) {
+            graph[edge.first].insert(edge.second);
+            graph[edge.second].insert(edge.first);
+        }
+        
+        int components = 0;
+        unordered_set<int> visited;
+        for (auto it = graph.begin(); it != graph.end(); ++it) {
+            if (!visited.count(it->first)) {
+                ++components;
+                if (!dfs(it->first, -1, graph, visited))
+                    return false;
+            }
+        }
+        return components == 1;
+    }
+
+    bool dfs(int node, int prev, unordered_map<int, unordered_set<int>> &graph, unordered_set<int> &visited) {
+        if (visited.count(node))
+            return false;
+
+        visited.insert(node);
+        for (auto it = graph[node].begin(); it != graph[node].end(); ++it) {
+            if (*it == prev)
+                continue;
+            if (!dfs(*it, node, graph, visited))
+                return false;
+        }
+        return true;
+    }
+};
+
 // [Solution]: Use level. If two nodes are each neighbors, if there level different by 1, they are parent-child. Otherwise there is a cycle.
 // [Corner Case]: Extra nodes that not connect to the graph
-class Solution {
+class SolutionSelfSolution {
 public:
     struct NodeLevel {
         int id, level;
@@ -63,7 +102,7 @@ int main() {
     //vector<pair<int, int>> edges = {{0, 1}, {1, 2}, {2, 3}, {1, 3}, {1, 4}};
     //vector<pair<int, int>> edges = {{0, 1}, {1, 3}, {3, 4}};
     vector<pair<int, int>> edges = {};
-    cout << sol.validTree(1, edges) << endl;
+    cout << sol.validTree(2, edges) << endl;
 
     return 0;
 }
