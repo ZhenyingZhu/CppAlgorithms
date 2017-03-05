0f072efbf2afcd3af102ed11f7cd2446fa875ca4
ZhenyingZhu <zz2283@columbia.edu>
2017-03-04 21:35:13 -0800

    Use standard DFS

--- src/leetcode/CourseSchedule.cpp
+++ src/leetcode/CourseSchedule.cpp
@@ -22,6 +22,42 @@ using namespace std;
 class Solution {
 public:
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        if (numCourses <= 1 || prerequisites.empty())
+            return true;
+
+        unordered_map<int, vector<int>> graph;
+        for (pair<int, int> &edge : prerequisites) {
+            graph[edge.first].push_back(edge.second);
+        }
+
+        unordered_set<int> visited, visiting;
+        for (auto it = graph.begin(); it != graph.end(); ++it) {
+            if (visited.count(it->first))
+                continue;
+            if (hasCycle(it->first, graph, visiting, visited))
+                return false;
+        }
+        return true;
+    }
+
+    bool hasCycle(int node, unordered_map<int, vector<int>> &graph, unordered_set<int> &visiting, unordered_set<int> &visited) {
+        if (visiting.count(node))
+            return true;
+
+        visiting.insert(node);
+        for (int &neighbor : graph[node]) {
+            if (hasCycle(neighbor, graph, visiting, visited))
+                return true;
+        }
+        visiting.erase(node);
+        visited.insert(node);
+        return false;
+    }
+};
+
+class SolutionChaos {
+public:
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
         if (numCourses == 0)
             return true;
 
@@ -67,10 +103,10 @@ private:
 int main() {
     Solution sol;
 
-    //vector<pair<int, int>> pre = {{0, 1}, {1, 0}};
+    vector<pair<int, int>> pre = {{0, 1}, {1, 0}};
     //vector<pair<int, int>> pre = {{0, 1}};
     //vector<pair<int, int>> pre = {{1, 0}, {2, 0}};
-    vector<pair<int, int>> pre = {{1,0},{2,6},{1,7},{6,4},{7,0},{0,5}};
+    //vector<pair<int, int>> pre = {{1,0},{2,6},{1,7},{6,4},{7,0},{0,5}};
     cout << (sol.canFinish(8, pre)?"True":"False") << endl;
 
     return 0;
