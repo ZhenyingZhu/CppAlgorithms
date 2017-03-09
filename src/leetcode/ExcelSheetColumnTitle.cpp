04c4c8bb89d59666f5d0745405c761ecbee82fd9
ZhenyingZhu <zz2283@columbia.edu>
2017-03-08 23:36:50 -0800

    Excel sheet column

--- src/leetcode/ExcelSheetColumnTitle.cpp
+++ src/leetcode/ExcelSheetColumnTitle.cpp
@@ -5,12 +5,24 @@
  */
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 // [Solution]: mode 26, then add a correspond letter until 0.
 // [Corner Case]: should treat A as 1 since otherwise AA become 0.
 class Solution {
+public:
+    string convertToTitle(int n) {
+        string res;
+        string candidates = "ZABCDEFGHIJKLMNOPQRSTUVWXY";
+        while (n != 0) {
+            res.push_back(candidates[n % 26]);
+            n =  (n - 1) / 26;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
 };
 
 // [Solution]:
@@ -20,6 +32,9 @@ https://github.com/ZhenyingZhu/ClassicAlgorithms/blob/master/src/algorithms/math
 
 int main() {
     Solution sol;
+
+    for (int i = 1; i <= 26*26; ++i)
+        cout << sol.convertToTitle(i) << endl;
 
     return 0;
 }
