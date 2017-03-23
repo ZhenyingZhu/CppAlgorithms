2ec42ad0e5f4c0be4888e385af039e55eb594a90
ZhenyingZhu <zz2283@columbia.edu>
2017-03-22 23:35:32 -0700

    Add a solution for Find k pairs wit hsmallest sums

--- src/leetcode/FindKPairsWithSmallestSums.cpp
+++ src/leetcode/FindKPairsWithSmallestSums.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 // [Solution]: Each time push a pair to result, get 1. the number from nums1 and the next number from nums2, and 2. the next number from nums1, and its unpushed pair in nums2. Push them to a priority queue.
 // [Corner Case]: Cannot use two hash maps to store the next idx for nums1 and nums2, because we are only traverse nums1.
-class Solution {
+class SolutionOld {
 public:
     struct PairIdx {
         int idx1, idx2;
@@ -70,10 +70,83 @@ cout << endl;
     }
 };
 
-// [Solution]: First push k elements from nums1 + nums2[0] to the pq, then just move nums2 pt ahead
-/* Java solution
+// [Solution]: Wrong because it push duplicate pairs, as (0,1) and (1,0) both add (1,1)
+class SolutionWrong {
+public:
+    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<pair<int, int>> res;
+        if (nums1.empty() || nums2.empty())
+            return res;
 
- */
+        priority_queue<IdxPair> pq;
+        pq.push( getIdx(nums1, nums2, 0, 0) );
+        while (!pq.empty() && (int)res.size() < k) {
+            IdxPair cur = pq.top();
+            pq.pop();
+
+cout << cur.idx1 << ", " << cur.idx2 << endl;
+            res.push_back( {nums1[cur.idx1], nums2[cur.idx2]} );
+
+            if (cur.idx2 + 1 < (int)nums2.size())
+                pq.push( getIdx(nums1, nums2, cur.idx1, cur.idx2 + 1) );
+            if (cur.idx1 + 1 < (int)nums1.size())
+                pq.push( getIdx(nums1, nums2, cur.idx1 + 1, cur.idx2) );
+        }
+        return res;
+    }
+
+private:
+    struct IdxPair {
+        int sum;
+        int idx1, idx2;
+        bool operator<(const IdxPair &other) const {
+            return sum > other.sum;
+        }
+    };
+
+    IdxPair getIdx(vector<int> &nums1, vector<int> &nums2, int idx1, int idx2) {
+        return { nums1[idx1] + nums2[idx2], idx1, idx2 };
+    }
+};
+
+// [Solution]: First push k elements from nums1 + nums2[0] to the pq, then just move nums2 pt ahead
+class Solution {
+public:
+    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<pair<int, int>> res;
+        if (nums1.empty() || nums2.empty())
+            return res;
+
+        priority_queue<IdxPair> pq;
+        for (int i = 0; i < k && i < (int)nums1.size(); i++)
+            pq.push( getIdx(nums1, nums2, i, 0) );
+
+        while (!pq.empty() && (int)res.size() < k) {
+            IdxPair cur = pq.top();
+            pq.pop();
+
+cout << cur.idx1 << ", " << cur.idx2 << endl;
+            res.push_back( {nums1[cur.idx1], nums2[cur.idx2]} );
+
+            if (cur.idx2 + 1 < (int)nums2.size())
+                pq.push( getIdx(nums1, nums2, cur.idx1, cur.idx2 + 1) );
+        }
+        return res;
+    }
+
+private:
+    struct IdxPair {
+        int sum;
+        int idx1, idx2;
+        bool operator<(const IdxPair &other) const {
+            return sum > other.sum;
+        }
+    };
+
+    IdxPair getIdx(vector<int> &nums1, vector<int> &nums2, int idx1, int idx2) {
+        return { nums1[idx1] + nums2[idx2], idx1, idx2 };
+    }
+};
 
 int main() {
     Solution sol;
