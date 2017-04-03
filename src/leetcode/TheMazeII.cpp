90b0f8ce798051b6ba3417ac0c2d07f42b79a732
zhenyinz <zhenyinz@amazon.com>
2017-04-02 18:32:21 -0700

    505 use DFS brute force

--- src/leetcode/TheMazeII.cpp
+++ src/leetcode/TheMazeII.cpp
@@ -1,122 +1,162 @@
 /*
- * [Source] https://leetcode.com/problems/the-maze-ii/
- * [Difficulty]: Hard
+ * [Source] https://leetcode.com/problems/
+ * [Difficulty]: 
  * [Tag]: 
  */
 
 #include <iostream>
+#include <climits>
 #include <vector>
 
 using namespace std;
 
-// [Solution]: Brute force check every direction. Need to avoid cycle.
+// [Solution]:
 // [Corner Case]:
 class Solution {
 public:
-    string findShortestWay(vector<vector<int>>& maze, vector<int>& ball, vector<int>& hole) {
-        maze[hole[0]][hole[1]] = 2; // tag the hole as 2
+    int shortestDistance(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        if (maze.empty() || maze[0].empty())
+            return 0;
+        initStopPos(maze);
 
-        int minDis = maze.size() * maze[0].size();
-        string res;
-        vector<char> movements;
-        for (char dir : "dlru") {
-            helper(ball, maze, dir, movements, 0, minDis, res);
+        vector<vector<int>> dp(maze.size(), vector<int>(maze[0].size(), INT_MAX)); // INT_MAX indicate not visited
+        int res = dfs(start[0], start[1], dp, maze, destination);
+ 
+        for (int i = 0; i < maze.size(); i++) {
+            for (int j = 0; j < maze[0].size(); j++) {
+                if (dp[i][j] == INT_MAX)
+                    cout << "b, \t";
+                else
+                    cout << dp[i][j] << ",\t";
+            }
+            cout << endl;
         }
-
-        if (res.empty())
-            return "impossible";
+        
         return res;
     }
 
-    struct MoveStatus {
-        int dis;
-        bool reachHole;
-    };
+private:
+    vector<vector<int>> up, down, left, right; // from a cell, where can it stop
 
-    void helper(vector<int> ball, vector<vector<int>> &maze, char dir, vector<char> &movements, int preDis, int &minDis, string &res) {
-        int oriX = ball[0], oriY = ball[1];
+    void initStopPos(vector<vector<int>> &maze) {
+        int h = maze.size(), w = maze[0].size();
+        up.resize(h, vector<int>(w));
+        down.resize(h, vector<int>(w));
+        left.resize(h, vector<int>(w));
+        right.resize(h, vector<int>(w));
 
-        MoveStatus ms = move(ball, maze, dir);
-        if (ms.dis == 0) // invalid move
-            return;
-
-        int distance = ms.dis + preDis;
-        if (distance > minDis)
-            return;
-
-        movements.push_back(dir);
-        if (ms.reachHole) {
-            if (distance < minDis) {
-                minDis = distance;
-                res = string(movements.begin(), movements.end());
+        // left
+        for (int i = 0; i < h; i++) {
+            int obstacle = -1;
+            for (int j = 0; j < w; j++) {
+                if (maze[i][j] == 1)
+                    obstacle = j;
+                else
+                    left[i][j] = obstacle + 1;
             }
-            movements.pop_back();
-            return;
         }
 
-        // tag the start point as visited
-        maze[oriX][oriY] = -1;
-        for (char newDir : getDir(dir)) {
-            helper(ball, maze, newDir, movements, distance, minDis, res);
+        // right
+        for (int i = 0; i < h; i++) {
+            int obstacle = w;
+            for (int j = w - 1; j >= 0; j--) {
+                if (maze[i][j] == 1)
+                    obstacle = j;
+                else
+                    right[i][j] = obstacle - 1;
+            }
         }
-        movements.pop_back();
-        maze[oriX][oriY] = 0;
+
+        // up
+        for (int j = 0; j < w; j++) {
+            int obstacle = -1;
+            for (int i = 0; i < h; i++) {
+                if (maze[i][j] == 1)
+                    obstacle = i;
+                else
+                    up[i][j] = obstacle + 1;
+            }
+        }
+
+        // down
+        for (int j = 0; j < w; j++) {
+            int obstacle = h;
+            for (int i = h - 1; i >= 0; i--) {
+                if (maze[i][j] == 1)
+                    obstacle = i;
+                else
+                    down[i][j] = obstacle - 1;
+            }
+        }
     }
 
-    MoveStatus move(vector<int> &ball, vector<vector<int>> &maze, char dir) {
-        int xInc = 0, yInc = 0;
-        if (dir == 'u') {
-            xInc = -1;
-        } else if (dir == 'd') {
-            xInc = 1;
-        } else if (dir == 'l') {
-            yInc = -1;
-        } else {
-            yInc = 1;
+    vector<int> getNext(int x, int y, int dir) {
+        // dir: 0 left, 1 down, 2 right, 3 up
+        switch(dir) {
+            case 0: return {x, left[x][y]};
+            case 1: return {down[x][y], y};
+            case 2: return {x, right[x][y]};
+            case 3: return {up[x][y], y};
+            default: return {};
         }
-
-        int &x = ball[0], &y = ball[1];
-        int dis = 0;
-        while (x >= 0 && x < (int)maze.size() && y >= 0 && y < (int)maze[0].size() && maze[x][y] == 0) {
-            ++dis;
-            x += xInc;
-            y += yInc;
-        }
-
-        if (x >= 0 && x < (int)maze.size() && y >= 0 && y < (int)maze[0].size()) {
-            if (maze[x][y] == -1)
-                return {0, false}; // cycle detected
-            if (maze[x][y] == 2)
-                return {dis, true}; // reach hole
-        }
-
-        // touch board or wall, need return back
-        --dis;
-        x -= xInc;
-        y -= yInc;
-        return {dis, false};
     }
 
-    string getDir(char dir) {
-        if (dir == 'u' || dir == 'd')
-            return "lr";
-        return "du";
+    int dfs(int x, int y, vector<vector<int>> &dp, vector<vector<int>> &maze, vector<int> &dest) {
+        if (dp[x][y] != INT_MAX)
+            return dp[x][y];
+
+        if (x == dest[0] && y == dest[1]) {
+            dp[x][y] = 1;
+            return 1;
+        }
+
+        maze[x][y] = 2;
+        for (int dir = 0; dir < 4; dir++) {
+            vector<int> next = getNext(x, y, dir);
+            if (maze[next[0]][next[1]] == 2) // cycle
+                continue;
+
+            int totalDis = dfs(next[0], next[1], dp, maze, dest);
+            if (totalDis == -1)
+                continue;
+
+            if (dir == 0)
+                totalDis += y - next[1];
+            else if (dir == 1)
+                totalDis += next[0] - x;
+            else if (dir == 2)
+                totalDis += next[1] - y;
+            else
+                totalDis += x - next[0];
+
+            dp[x][y] = min(dp[x][y], totalDis);
+        }
+        maze[x][y] = 0;
+
+        if (dp[x][y] == INT_MAX)
+            dp[x][y] = -1;
+        return dp[x][y];
     }
 };
+
+// [Solution]:
+/* Java solution
+
+ */
 
 int main() {
     Solution sol;
 
     vector<vector<int>> maze = {
+        {0,0,1,0,0},
         {0,0,0,0,0},
-        {1,1,0,0,1},
-        {0,0,0,0,0},
-        {0,1,0,0,1},
-        {0,1,0,0,0}
+        {0,0,0,1,0},
+        {1,1,0,1,1},
+        {0,0,0,0,0}
     };
+    vector<int> start = {0, 4}, dest = {4,4};
 
-    vector<int> ball = {4,3}, hole = {0,1};
-    cout << sol.findShortestWay(maze, ball, hole) << endl;
+    cout << sol.shortestDistance(maze, start, dest) << endl;
 
     return 0;
 }
